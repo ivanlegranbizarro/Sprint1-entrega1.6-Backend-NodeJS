2ef6e532a52bc67e4e7361fc20a5755746d09a94
Iván Legrán <68439095+ivanlegranbizarro@users.noreply.github.com>
2022-10-17 11:14:42 +0200

    Nivell 1 complet

---
+++ app/funcions.js
@@ -0,0 +1,94 @@
+/*
+Crea una funció que retorni una Promise que invoqui la funció resolve() o reject() que rep. Invoca-la passant-li les dues funcions de manera que imprimeixin un missatge diferent depenent de si la Promise es resol o no.
+*/
+
+function retornaPromesa(num) {
+  return new Promise((resolve, reject) => {
+    if (num > 0.5) {
+      resolve("La promesa s'ha resolt correctament amb el número " + num);
+    } else {
+      reject("La promesa no s'ha pogut resoldre amb el número " + num);
+    }
+  });
+}
+
+/*
+Crea una arrow function que rebi un paràmetre i una funció callback i li passi a la funció un missatge o un altre (que s'imprimirà per consola) en funció del paràmetre rebut
+*/
+
+const arrowFunction = (param, callback) => {
+  if (param == 'Iván') {
+    return callback('Hola, Iván');
+  } else {
+    return callback('Hola, desconegut');
+  }
+};
+
+/*
+Donats els objectes employees i salaries, crea una arrow function getEmployee() que retorni una Promise efectuant la cerca en l'objecte pel seu id.
+*/
+
+let employees = [
+  {
+    id: 1,
+    name: 'Linux Torvalds',
+  },
+  {
+    id: 2,
+    name: 'Bill Gates',
+  },
+  {
+    id: 3,
+    name: 'Jeff Bezos',
+  },
+];
+
+let salaries = [
+  {
+    id: 1,
+    salary: 4000,
+  },
+  {
+    id: 2,
+    salary: 1000,
+  },
+];
+
+const getEmployee = (id) => {
+  return new Promise((resolve, reject) => {
+    let employee = employees.find((e) => e.id === id);
+    if (employee) {
+      resolve(employee);
+    } else {
+      reject("No s'ha trobat l'empleat");
+    }
+  });
+};
+
+/*
+  Crea una altra arrow function getSalary() similar a l'anterior que rebi com a paràmetre un objecte employee i retorni el seu salari.
+  */
+
+const getSalary = (empleat) => {
+  return new Promise((resolve, reject) => {
+    if (empleat) {
+      let salary = salaries.find((s) => s.id === empleat.id);
+      if (salary) {
+        resolve(salary);
+      } else {
+        reject("No s'ha trobat el salari");
+      }
+    } else {
+      reject("No s'ha trobat l'empleat");
+    }
+  });
+};
+
+module.exports = {
+  retornaPromesa,
+  arrowFunction,
+  getEmployee,
+  getSalary,
+  employees,
+  salaries,
+};
---
+++ tests/funcions.spec.js
@@ -0,0 +1,78 @@
+const {
+  retornaPromesa,
+  arrowFunction,
+  getEmployee,
+  getSalary,
+  employees,
+  salaries,
+} = require('../app/funcions');
+
+// retornaPromesa
+describe('Comprovem que la funció retornaPromesa compleixi amb les expectatives', () => {
+  test('should resolve or reject the promise', () => {
+    expect(retornaPromesa(0.6)).resolves.toEqual(
+      expect.stringContaining("La promesa s'ha resolt correctament")
+    );
+  });
+  test('should resolve or reject the promise', () => {
+    expect(retornaPromesa(0.4)).rejects.toEqual(
+      expect.stringContaining("La promesa no s'ha pogut resoldre")
+    );
+  });
+});
+
+// arrowFunction
+describe('Comprovem que la funció arrowFunction compleixi amb les expectatives', () => {
+  test('should return a message depending on the parameter passed', () => {
+    expect(arrowFunction('Iván', (message) => message)).toEqual(
+      expect.stringContaining('Hola, Iván')
+    );
+  });
+
+  test('should return a message depending on the parameter passed', () => {
+    expect(arrowFunction('Pepe', (message) => message)).toEqual(
+      expect.stringContaining('Hola, desconegut')
+    );
+  });
+});
+
+// getEmployee
+describe('Comprovem que la funció getEmployee compleixi amb les expectatives', () => {
+  test('should return an object with the employee data', () => {
+    expect(getEmployee(1)).resolves.toMatchObject({
+      id: 1,
+      name: 'Linux Torvalds',
+    });
+  });
+
+  test('should reject the promise if the employee does not exist', () => {
+    expect(getEmployee(4)).rejects.toEqual(
+      expect.stringContaining("No s'ha trobat l'empleat")
+    );
+  });
+});
+
+// getSalary
+describe('Comprovem que la funció getSalary compleixi amb les expectatives', () => {
+  test('should return an object with the salary data', () => {
+    const empleat = employees[0];
+    expect(getSalary(empleat)).resolves.toMatchObject({
+      id: 1,
+      salary: 4000,
+    });
+  });
+
+  test('should reject the promise if the salary does not exist', () => {
+    const empleat = employees[2];
+    expect(getSalary(empleat)).rejects.toEqual(
+      expect.stringContaining("No s'ha trobat el salari")
+    );
+  });
+
+  test('should reject the promise if the employee does not exist', () => {
+    const empleat = employees[3];
+    expect(getSalary(empleat)).rejects.toEqual(
+      expect.stringContaining("No s'ha trobat l'empleat")
+    );
+  });
+});
